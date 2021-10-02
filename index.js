70421f18df0465dd22144c5118c9669628fb8dca
annerachelpinto <annerachelpinto@gmail.com>
2021-10-02 11:54:46 +0530

    make changes in index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log('Hey this is Rachel in her branch');
+
+console.log('Editing more to create changes to create conflict');
 console.log('Hey this is Anne and i also wanted to change the same file');
